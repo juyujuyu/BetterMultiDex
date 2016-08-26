af58e60062b27db678eef894ce41f7d68e226431
lincunkun <lincunkun@youmi.net>
2016-08-26 10:16:35 +0800

    写了最基本的代码

---
+++ app/src/main/java/com/boltfish/multidex/App.java
@@ -0,0 +1,16 @@
+package com.boltfish.multidex;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+/**
+ * Created by yutou on 2016/8/24.
+ */
+public class App extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+}
